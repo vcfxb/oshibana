43c2355b36bc8a7ab1612ea58243d0b2adedbc72
Venus Xeon-Blonde <venus@vcfxb.org>
2026-02-05 00:38:42 -0500

    navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,16 @@
 <script lang="ts">
+	import * as NavigationMenu from "$lib/components/ui/navigation-menu/index.js";
+	import { IsMobile } from "$lib/hooks/is-mobile.svelte.js";
+	import { ModeWatcher } from "mode-watcher";
 	import './layout.css';
 
+  	const isMobile = new IsMobile();
 	let { children } = $props();
+	let wordmarkSrc = "/iconography/wordmark-white.svg"; // todo: save themes in db, handle with db
+
+	// $effect(() => {
+	// 	wordmarkSrc = mode.current === "dark" ? "/iconography/wordmark-white.svg" : "/iconography/wordmark-black.svg";
+	// });
 </script>
 
 <svelte:head>
@@ -9,6 +18,28 @@
 	<link rel="icon" href="iconography/favicon-light.svg" media="(prefers-color-scheme: dark)"/>
 </svelte:head>
 
+<ModeWatcher track={false} defaultMode={"dark"} />
 
+<NavigationMenu.Root viewport={isMobile.current} class="relative z-10 py-3 px-4 flex w-full justify-center">
+	<NavigationMenu.List class="group flex list-none items-center justify-center p-1">
+		<NavigationMenu.Item class="mx-5 mr-10">
+			<NavigationMenu.Link href="/">
+				<img src={wordmarkSrc} class="h-8" alt="Oshibana Wordmark"/>
+			</NavigationMenu.Link>
+		</NavigationMenu.Item>
+	</NavigationMenu.List>
+	<NavigationMenu.List class="p-1 group flex list-none items-center justify-center rounded-md flex-wrap px-2 bg-blue-200 dark:bg-blue-950">
+		<NavigationMenu.Item>
+			<NavigationMenu.Link class="text-lg m-1 hover:bg-blue-300 dark:hover:bg-blue-800" href="/decks">
+				Decks
+			</NavigationMenu.Link>
+		</NavigationMenu.Item>
+		<NavigationMenu.Item>
+			<NavigationMenu.Link class="text-lg m-1 hover:bg-blue-300 dark:hover:bg-blue-800" href="/users">
+				Users
+			</NavigationMenu.Link>
+		</NavigationMenu.Item>
+	</NavigationMenu.List>
+</NavigationMenu.Root>
 
 {@render children()}
--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,11 @@
 import adapter from '@sveltejs/adapter-cloudflare';
 
 /** @type {import('@sveltejs/kit').Config} */
-const config = { kit: { adapter: adapter() } };
+const config = { 
+    kit: { adapter: adapter() },
+    optimizeDeps: {
+        exclude: ['layercake']
+    },
+};
 
 export default config;
