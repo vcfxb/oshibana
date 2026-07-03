a493f706c77d93d5f27919476cae96d002a27a0c
Venus Xeon-Blonde <venus@vcfxb.org>
2026-07-03 19:17:41 -0400

    chore: cargo fmt

--- desktop/build.rs
+++ desktop/build.rs
@@ -1,8 +1,8 @@
-
 fn main() {
     built::write_built_file().expect("built tool");
 
-    #[cfg(windows)] {
+    #[cfg(windows)]
+    {
         let mut res = winres::WindowsResource::new();
         res.set_icon("assets/favicon/icons/icon.ico");
         res.compile().expect("winres");
